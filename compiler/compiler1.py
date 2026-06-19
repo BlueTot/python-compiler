75475e9568cfa681b8f76b7dddc070437e021e00
Nok Hang Lo <bluetotkc@gmail.com>
2026-06-19 01:12:04 +0100

    feat: start building AST

--- compiler/compiler1.py
+++ compiler/compiler1.py
@@ -1,11 +1,90 @@
 from lark import Lark, Transformer, v_args
+from dataclasses import dataclass
+
+    
+
+@dataclass
+class Block:
+    statements: list[int]
+
+@dataclass
+class Assignment:
+    var_name: str
+    value: Expression
+
+@dataclass
+class IfStatement:
+    condition: Condition 
+    true_branch: Block
+    false_branch: Block | None
+
+
+
+
+
+
+
 
 @v_args(inline=True)
 class ASTBuilder(Transformer):
+
     """
         builds the AST
     """
-    pass
+
+    def block(self, *statements) -> None:
+        pass
+
+    def assignment(self, var_name, var_expression) -> None:
+        pass
+
+    def if_statement(self, condition, if_block) -> None:
+        pass
+
+    def while_statement(self, condition, loop_block) -> None:
+        pass
+
+    def for_statement(self, initial, condition, increment, loop_block) -> None:
+        pass
+
+    def greater_than(self, expr1, expr2) -> None:
+        pass
+
+    def less_than(self, expr1, expr2) -> None:
+        pass
+
+    def not_equal_to(self, expr1, expr2) -> None:
+        pass
+
+    def greater_than_equal(self, expr1, expr2) -> None:
+        pass
+
+    def less_than_equal(self, expr1, expr2) -> None:
+        pass
+
+    def add(self, expr1, expr2) -> None:
+        pass
+
+    def sub(self, expr1, expr2) -> None:
+        pass
+
+    def mul(self, expr1, expr2) -> None:
+        pass
+
+    def mul(self, expr1, expr2) -> None:
+        pass
+
+    def div(self, expr1, expr2) -> None:
+        pass
+
+    def number(self, value) -> None:
+        pass
+
+    def neg(self, expr) -> None:
+        pass
+
+    def var(self, name) -> None:
+        pass
 
 
 
