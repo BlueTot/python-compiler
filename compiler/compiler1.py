4814803719bb45d4e739242b79e5b5d6ea060311
BlueTot <bluetotkc@gmail.com>
2026-06-19 22:54:20 +0100

    feat: add data types to grammar

--- compiler/compiler1.py
+++ compiler/compiler1.py
@@ -1,107 +1,144 @@
 from lark import Lark, Transformer, v_args
 from dataclasses import dataclass
+from enum import Enum
 
     
 """
 Dataclasses for AST nodes
 """
 
-
+# arithemtic expression
 @dataclass
 class Expression:
     pass
 
+# addition of two expressions
 @dataclass
 class AddExpression(Expression):
     expr1: Expression
     expr2: Expression
 
+# subtraction of two expressions
 @dataclass
 class SubExpression(Expression):
     expr1: Expression
     expr2: Expression
 
+# multiplication of two expressions
 @dataclass
 class MulExpression(Expression):
     expr1: Expression
     expr2: Expression
 
+# division of two expressions
 @dataclass
 class DivExpression(Expression):
     expr1: Expression
     expr2: Expression
 
+# negation of an expression
 @dataclass
 class NegatedExpression(Expression):
     expr: Expression
 
+# number node
 @dataclass
 class Number(Expression):
     value: int
 
+# variable node
 @dataclass
 class Variable(Expression):
     name: str
 
+# condition built out of expressions
 @dataclass
 class Condition:
     pass
 
+# condition with > operator
 @dataclass
-class GreaterThanCondition:
+class GreaterThanCondition(Condition):
     expr1: Expression
     expr2: Expression
 
+# condition with < operator
 @dataclass
-class LessThanCondition:
+class LessThanCondition(Condition):
     expr1: Expression
     expr2: Expression
 
+# condition with == operator
 @dataclass
-class EqualsCondition:
+class EqualsCondition(Condition):
     expr1: Expression
     expr2: Expression
 
+# condition with != operator
 @dataclass
-class NotEqualsCondition:
+class NotEqualsCondition(Condition):
     expr1: Expression
     expr2: Expression
 
+# condition with >= operator
 @dataclass
-class GreaterThanOrEqualCondition:
+class GreaterThanOrEqualCondition(Condition):
     expr1: Expression
     expr2: Expression
 
+# condition with <= operator
 @dataclass
-class LessThanOrEqualCondition:
+class LessThanOrEqualCondition(Condition):
     expr1: Expression
     expr2: Expression
 
+# statement
+@dataclass
+class Statement:
+    pass
+
+# block of statements
 @dataclass
 class Block:
-    statements: list[int]
+    statements: list[Statement]
 
+# data type
+class DataType(Enum):
+    INT_TYPE = 1
+    BOOL_TYPE = 1
+
+# variable declaration
 @dataclass
-class Assignment:
+class VarDeclaration(Statement):
+    datatype: DataType
+    var_name: str
+    value: Expression | None
+
+# assignment statement
+@dataclass
+class Assignment(Statement):
     var_name: str
     value: Expression
 
+# if statement
 @dataclass
-class IfStatement:
+class IfStatement(Statement):
     condition: Condition 
     true_branch: Block
     false_branch: Block | None
 
+# while loop statement
 @dataclass
-class WhileStatement:
+class WhileStatement(Statement):
     condition: Condition
     loop_body: Block
 
+# for loop statement
 @dataclass
-class ForStatement:
-    initial: Assignment
+class ForStatement(Statement):
+    initial: VarDeclaration | Statement
     condition: Condition
-    increment: Assignment
+    increment: Statement 
     loop_body: Block
 
 
@@ -119,6 +156,18 @@ class ASTBuilder(Transformer):
 
     def block(self, *statements) -> Block:
         return Block(list(statements))
+
+    def int_type(self) -> DataType:
+        return DataType.INT_TYPE
+
+    def bool_type(self) -> DataType:
+        return DataType.BOOL_TYPE
+
+    def var_declaration(self, datatype, var_name) -> VarDeclaration:
+        return VarDeclaration(datatype, var_name, None)
+
+    def var_declaration_with_value(self, datatype, var_name, value) -> VarDeclaration:
+        return VarDeclaration(datatype, var_name, value)
 
     def assignment(self, var_name, var_expression) -> Assignment:
         return Assignment(var_name, var_expression)
