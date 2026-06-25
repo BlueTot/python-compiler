7ad056e11ee9f68ee4bc9588c950dc46a9676265
Nok Hang Lo <bluetotkc@gmail.com>
2026-06-25 16:06:35 +0100

    feat(compiler): tidy up entry point

--- compiler/main.py
+++ compiler/main.py
@@ -1,6 +1,6 @@
 from lark import Lark
-from parser import ASTBuilder
-from semantic import SemanticChecker
+from parser import ASTBuilder, CompileError
+from semantic import SemanticChecker, SemanticError
 
 
 """
@@ -17,15 +17,30 @@ code generation -> assembly code
 parser = Lark.open("./grammar.lark", parser="lalr", transformer=ASTBuilder())
 
 
-def main() -> None:
-    with open("./code.txt") as f:
-        program = f.read()
+def compile(program: str) -> None:
+    """
+        Main entry point to the program
+        Takes a program as string and performs parsing and semantic analysis
+        
+        prints "All Ok" if no errors, 
+        "SemanticError: ..." if there is a semantic error, or 
+        "CompileError: ..." if there is a compile (parsing) error
+    """
 
-    ast = parser.parse(program)
-    # print(ast)
+    try:
 
-    SemanticChecker().check(ast)
+        ast = parser.parse(program)
+        SemanticChecker().check(ast)
+        print("All Ok")
+
+    except CompileError as e:
+        print(f"CompileError: {e}")
+    except SemanticError as e:
+        print(f"SemanticError: {e}")
     
 
+
 if __name__ == "__main__":
-    main()
+    with open("./code.txt") as f:
+        program = f.read()
+    compile(program)
